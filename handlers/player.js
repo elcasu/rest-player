0ce574d1912c4a745d502db03b14b2050cfc745a
Gabriel Casullo <info@casu-net.com.ar>
2017-12-17 22:42:06 -0300

    add some controls

--- handlers/player.js
+++ handlers/player.js
@@ -12,12 +12,18 @@ module.exports = class PlayerController {
     // get video list
     router.get('/', ErrorHandler(this.getList))
 
+    // get video info
+    router.get('/:id', ErrorHandler(this.info))
+
     // play video
     router.post('/:id/play', ErrorHandler(this.startVideo))
 
     // Controls
+    router.post('/:id/back', ErrorHandler(this.back))
     router.post('/:id/fwd', ErrorHandler(this.fwd))
     router.post('/:id/stop', ErrorHandler(this.stop))
+    router.post('/:id/volUp', ErrorHandler(this.volUp))
+    router.post('/:id/volDown', ErrorHandler(this.volDown))
 
     app.use('/api/videos', router)
   }
@@ -29,9 +35,13 @@ module.exports = class PlayerController {
   async startVideo (req, res) {
     const video = await Video.findById(req.params.id)
     player = Omx(video.path, 'hdmi', false, 100)
-    // player.openFile(video.path)
-    // player.play()
     res.send({ message: `Playing ${video.name}` })
+  }
+
+  async back (req, res) {
+    const video = await Video.findById(req.params.id)
+    player.back30()
+    res.send({ message: `back 30 sec ${video.name}` })
   }
 
   async fwd (req, res) {
@@ -44,5 +54,23 @@ module.exports = class PlayerController {
     const video = await Video.findById(req.params.id)
     player.quit()
     res.send({ message: `${video.name} stopped` })
+  }
+
+  async volUp (req, res) {
+    const video = await Video.findById(req.params.id)
+    player.volUp()
+    res.send({ message: `${video.name} - volume up` })
+  }
+
+  async volDown (req, res) {
+    const video = await Video.findById(req.params.id)
+    player.volDown()
+    res.send({ message: `${video.name} - volume down` })
+  }
+
+  async info (req, res) {
+    const video = await Video.findById(req.params.id)
+    const info = player.info()
+    res.send({ message: `${video.name} - video information`, info })
   }
 }
